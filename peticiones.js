bb90e82cf9030460187e61dcc414c46dfeed7f0a
K. Soto <ksotoc@fcpn.edu.bo>
2025-10-17 13:34:20 -0400

    avance 2

--- peticiones.js
+++ peticiones.js
@@ -16,3 +16,25 @@ fetch('https://jsonplaceholder.typicode.com/posts')
     })
     .catch(e => console.error('Error: ', e))
 
+// Con fetch anidado
+//height, id, sprites.front_default, weight
+fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(response => response.json())
+    .then(data => {
+        const promesas = r.results.map(poke => 
+            fetch(poke.url)
+            .then(r2 = r2.json())
+        )
+        Promise.all(promesas)
+            .then(d => {
+                const filtrado = data.map(pk => ({
+                nombre: pk.name,
+                id: pk.id,
+                altura: pk.height,
+                peso: pk.weight
+            }))
+            console.log(d)
+        })
+        
+    })
+    .catch(e => console.error('Error: ', e)) 
